d7e15acd7a90ceb06d6939466d1567b0e0a3268e
yossi-gold <2048yossi@gmail.com>
2025-11-16 18:35:29 -0500

    Update db.js

--- backend/services/db.js
+++ backend/services/db.js
@@ -1,22 +1,16 @@
-import {Pool} from 'pg';
+
 import knex from 'knex';
 
 
-const client = new Pool({
-    connectionString: process.env.PG_CONNECTION_STRING
-})
 
-client.connect()
 
 
 const knexClient = knex({
-    client: 'pg',
-    connection: process.env.PG_CONNECTION_STRING,
-    pool: {
-        min: 3,
-        max: 10
-    }
+  client: 'pg',
+  connection: process.env.DATABASE_URL,
+  pool: { min: 3, max: 10 },
 });
+
 
 
 export default knexClient;
