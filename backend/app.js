8d346229d6ef42ff0abac4e3047c58abfbf55f6f
yossi-gold <2048yossi@gmail.com>
2025-11-17 15:49:41 -0500

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -28,7 +28,7 @@ app.use((req, res, next) => {
     
 
     try {
-        console.log(req.cookies,'jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj' )
+       
         const token = req.cookies.authToken;
 
     if (!token) {
