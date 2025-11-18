fba0f4fb69ae51e82ba4d9de62ee5fdfc43d1802
yossi-gold <2048yossi@gmail.com>
2025-11-18 15:20:42 -0500

    Delete frontend/src/assets directory

--- frontend/src/assets/context.jsx
+++
@@ -1,9 +0,0 @@
-import { createContext, useContext } from 'react';
-
-export const ProjectContext = createContext();
-
-
-
-export function useProjectInfo() {
-  return useContext(ProjectContext);
-}
